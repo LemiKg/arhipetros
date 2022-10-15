d7141cb569601999fdb400b4f3064e79f7120bb7
Lemixkg <rajevic.mile6@gmail.com>
2022-10-15 16:07:36 +0200

    adds UI improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./*.html'],
-  content: ['./*html'],
+  mode: "jit",
+  purge: ["./*.html"],
+  content: ["./*html"],
   theme: {
     extend: {
       backgroundImage: {
-        'body-background': 'url("../assets/images/pozadina.webp")'
+        "body-background": 'url("../assets/images/pozadina.webp")',
       },
       colors: {
-        // 'background': '#000',
-        // 'base-color': 'rgb(100, 34, 63)',
-        // 'text-color': '#fff',
-        // 'base-background': '#fff',
-        // 'menu-icon': '#fff',
-        // 'menu-shadow': '#e5e5e5',
-        // 'side-divider': '#2146a1',
-
-        'dark-color': '#100F0F',
-        'primary-color': '#0F3D3E',
-        'light-color': '#F1F1F1',
-        'secondary-color': '#E2DCC8'
+        "after-darken-color": "rgba(0, 0, 0, 0.6)",
+        "dark-color": "#100F0F",
+        "primary-color": "#0F3D3E",
+        "light-color": "#F1F1F1",
+        "secondary-color": "#E2DCC8",
       },
       gridTemplateColumns: {
-        'content-layout': 'auto 300px'
+        "content-layout": "auto 300px",
       },
       gridTemplateRows: {
-        'layout-2-two-cards': 'repeat(1, min-content)',
-        'layout-2-four-cards': 'repeat(2, min-content)',
-        'layout-2-six-cards': 'repeat(3, min-content)',
-        'layout-2-eight-cards': 'repeat(4, min-content)'
-      }
+        "layout-2-two-cards": "repeat(1, min-content)",
+        "layout-2-four-cards": "repeat(2, min-content)",
+        "layout-2-six-cards": "repeat(3, min-content)",
+        "layout-2-eight-cards": "repeat(4, min-content)",
+      },
     },
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     height: {
-      'logo-height': '30px',
-      'small-card-height': 'fit-content'
+      "logo-height": "30px",
+      "small-card-height": "fit-content",
     },
     minWidth: {
-      'small-card-width': '120px'
-    }
+      "small-card-width": "120px",
+    },
   },
   plugins: [],
-}
+};
